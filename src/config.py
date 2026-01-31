127da59133879c6f94ed34ac3acb67a52efaa6eb
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-02-01 00:42:11 +0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class SKConfig:
+    M = 2
+    G = 1
+    r = 16
+
+    kernel_sizes = (3, 5)
+    dilations = (1, 2)
+
+    in_channels = 64
+    num_classes = 1000
